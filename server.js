9c2b65521c3bf63fe0769c5d057e2b99a49a40f5
dan-th3man <144754769+dan-th3man@users.noreply.github.com>
2025-06-17 13:45:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+const http = require("http");
+const fs = require("fs");
 
+const port = 3000; 
+const server = http.createServer();
+ 
+server.on("listening", () => 
+    console.log(`Server running on port ${port}`)
+);
+
+server.on("request", (req, res) => {
+    res.writeHead(200, {"Content-type":"text/html"});
+	const url = req.url
+	console.log(url);
+  //Display website
+	fs.createReadStream("./index.html").pipe(res);
+});
+
+server.listen(port);
